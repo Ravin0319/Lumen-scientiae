709fcfbd5b8e94dcf5e9617869c6a1e7849b5d04
Ravindra <ravindra0319@gmail.com>
2017-02-19 01:10:57 +0530

    content update
    About us included

--- script.js
+++ script.js
@@ -10,6 +10,10 @@
 				templateUrl : 'pages/home.html',
 				controller  : 'mainController'
 			})
+			.when('/main', {
+				templateUrl : 'pages/main.html',
+				controller  : 'main2Controller'
+			})
 
 			// route for the about page
 			.when('/about', {
@@ -39,18 +43,55 @@
 		//$scope.message = 'Home Page - Logo comes here!';
 	});
 
+	scotchApp.controller('main2Controller', function($scope) {
+		
+		$(".mainwrap").addClass("animated slideInUp");
+		//$scope.message = 'Look! I am an about page.';
+	});
 	scotchApp.controller('aboutController', function($scope) {
+		$(".mainwrap").addClass("animated slideInUp");
 		//$scope.message = 'Look! I am an about page.';
 	});
 
 	scotchApp.controller('joinController', function($scope) {
 		//$scope.message = 'Contact us! JK. This is just a demo.';
+		$(".mainwrap").addClass("animated slideInUp");
 	});
 
 	scotchApp.controller('contactController', function($scope) {
 		//$scope.message = 'Contact us! JK. This is just a demo.';
+		$(".mainwrap").addClass("animated slideInUp");
 	});
 
 	scotchApp.controller('goodiesController', function($scope) {
+		$(".mainwrap").addClass("animated slideInUp");
 		//$scope.message = 'Contact us! JK. This is just a demo.';
 	});
+
+
+function enterLS() { 
+	document.getElementById("visit").className = "animated slideOutUp";
+
+	setTimeout(function(){
+		$(".VisitBgImg").addClass("animated slideOutDown");
+
+}, 1000);
+}
+
+$(document).ready(function() { 
+
+setTimeout(function(){
+	 	$("#visitButton").addClass("animated jello");
+	 }, 1000);  
+
+});
+
+
+
+
+
+
+
+
+
+
